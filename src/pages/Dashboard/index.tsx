d05fe0ef1977aa555af84b6a62f52dc8ba7a00d3
Gustavo Gomes <gustavo.gomesspi@gmail.com>
2021-01-08 15:46:57 -0300

    aplicando o styled components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Title } from './styles'
 
 const Dashboard: React.FC = () => {
-    return <h1> Dashbord </h1>
+    return <Title> Explore repositórios no Github </Title>
 }
 
 export default Dashboard;
