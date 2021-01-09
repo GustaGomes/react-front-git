becf81d7c831928ee07cf62a22c8433d71d16fa3
Gustavo Gomes <gustavo.gomesspi@gmail.com>
2021-01-09 03:31:35 -0300

    desenvolvendo layout dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,67 @@
 import React from 'react';
-import { Title } from './styles'
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories} from './styles';
 
 const Dashboard: React.FC = () => {
-    return <Title> Explore repositórios no Github </Title>
-}
+    return (
+        <>
+        <img src={logoImg} alt=""/>
+        <Title> Explore repositórios no Github </Title>
 
+        <Form>
+            <input placeholder="Digite o nome do repositório" />
+            <button type="submit"> Pesquisar </button>
+        </Form>
+
+        <Repositories>
+
+            <a href="">
+                <img 
+                src="https://avatars3.githubusercontent.com/u/26532535?s=460&u=740ecd46ee025cf92ba253ee0c96057d5ca5ac4a&v=4" 
+                alt="Gomes Gustavo"
+                />
+
+                <div>
+                    <strong> Gustavo Gomes | ReactJS | Angular </strong>
+                    <p> Easy peasy highly scalable ReacleJS & React Native forms! </p>
+                </div>
+
+                <FiChevronRight size={20} />
+            </a>
+
+            <a href="">
+                <img 
+                src="https://avatars3.githubusercontent.com/u/26532535?s=460&u=740ecd46ee025cf92ba253ee0c96057d5ca5ac4a&v=4" 
+                alt="Gomes Gustavo"
+                />
+
+                <div>
+                    <strong> Gustavo Gomes | ReactJS | Angular </strong>
+                    <p> Easy peasy highly scalable ReacleJS & React Native forms! </p>
+                </div>
+
+                <FiChevronRight size={20} />
+            </a>
+
+            <a href="">
+                <img 
+                src="https://avatars3.githubusercontent.com/u/26532535?s=460&u=740ecd46ee025cf92ba253ee0c96057d5ca5ac4a&v=4" 
+                alt="Gomes Gustavo"
+                />
+
+                <div>
+                    <strong> Gustavo Gomes | ReactJS | Angular </strong>
+                    <p> Easy peasy highly scalable ReacleJS & React Native forms! </p>
+                </div>
+
+                <FiChevronRight size={20} />
+            </a>
+
+        </Repositories>
+        </>
+    );
+};
 export default Dashboard;
