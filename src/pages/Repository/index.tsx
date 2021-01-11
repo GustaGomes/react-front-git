8ca52da3057e2f61ac40d1340fa5639fa61ab43e
Gustavo Gomes <gustavo.gomesspi@gmail.com>
2021-01-11 20:34:14 -0300

    colocando estilo na pagina de estilo

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues} from './styles';
 
 interface RepositoryParams {
     repository: string;
@@ -7,7 +11,52 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
-    return <h1> Repository:{params.repository} </h1>
+    return (
+        <>
+            <Header> 
+                <img src={logoImg} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+                {/* Repository:{params.repository}  */}
+            </Header>
+            <RepositoryInfo>
+                <header>
+                    <img src="https://avatars1.githubusercontent.com/u/26532535?v=4" alt="Img minha"/>
+                    <div>
+                        <strong> react/reacttop</strong>
+                        <p> curso top pra mim mesmo </p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>68</strong>
+                        <span>Issues</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link to="ggdfgf">
+                    <div>
+                        <strong>hdfhgd </strong>
+                        <p> gfhfdhhg </p>
+                    </div>
+    
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    );
 };
 
 export default Repository;
